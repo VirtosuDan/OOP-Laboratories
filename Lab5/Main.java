d75bf425eba98ecb9e13d0201319464672f16b53
VirtosuDan <45829623+VirtosuDan@users.noreply.github.com>
2019-12-10 20:29:34 +0200

    Add files via upload

---
+++ Lab5/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] argv) {
+        Class_J j = new Class_J("class_a", "class_b", "class_c", "class_d", "class_e", "class_f", "class_g", "class_h", "class_i", "class_j", new X("The Name for X"));
+
+        j.printProperty();
+    }
+}
